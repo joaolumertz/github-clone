ef3032997c6ea77e2f6f0971a8373d33fc10c6af
Kagami <cover4772@gmail.com>
2020-10-02 03:58:30 -0300

    sla

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { 
+  Container,
+  Line,
+  GithubLogo,
+} from './styles';
+
+const Footer: React.FC = () => {
+  return (
+    <Container>
+      <Line />
+      <GithubLogo />
+    </Container>
+  );
+}
+
+export default Footer;
